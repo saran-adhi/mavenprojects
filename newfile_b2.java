0590bc05b0ff0720bd31051064237c5e9aad05d4
saranadhi <saranaadithyanv@gmail.com>
2024-03-15 20:02:58 +0530

    Second Push

--- newfile_b2.java
+++ newfile_b2.java
@@ -1,7 +1,11 @@
 class Sample{
+
+public static int add(int a,int b){
+return a+b;
+}
 public static void main(String[] args){
 int a = 1;
 int b= 2;
-  System.out.println("Addition : ",a+b);
+  System.out.println("Addition : ",add(a,b));
 }
 }
